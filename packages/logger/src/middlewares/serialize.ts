1bc41824aeaa1c46a9d9930165db85c785ec5f3e
unadlib <unadlib@gmail.com>
2025-03-26 14:49:03 +0800

    fix(logger): fix logger data structure

--- packages/logger/src/middlewares/serialize.ts
+++ packages/logger/src/middlewares/serialize.ts
@@ -10,14 +10,15 @@ export class SerializeMiddlewares implements IMiddlewares {
   }
 
   handleParams(params: any[]) {
-    const arr = params.map((item) =>
-      item instanceof Error ? serializeError(item) : item
-    );
+    let arr: any[] = [];
     try {
-      return stringify(arr).slice(1, -1);
+      arr = params.map((item) =>
+        item instanceof Error ? serializeError(item) : item
+      );
+      return stringify(arr);
     } catch (e) {
-      console.error(e);
-      return Array.isArray(arr) && typeof arr[0] === 'string'
+      console.trace(e);
+      return typeof arr[0] === 'string'
         ? `['${arr[0]}', 'non-serializable data']`
         : 'non-serializable data';
     }
